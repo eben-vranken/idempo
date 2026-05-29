c1cc922dcd0347ce180cf331a2f7f2df4079e759
eben-vranken <vranken.eben@outlook.com>
2026-05-29 20:09:16 +0200

    feat: initalize in-memory store structure

--- inmem/store.go
+++ inmem/store.go
@@ -1,1 +1,26 @@
 package inmem
+
+import (
+	"sync"
+	"time"
+)
+
+type Entry struct {
+	bodyHash     string
+	state        string
+	responseCode int
+	responseBody []byte
+	expiryTime   time.Time
+}
+
+type InMemStore struct {
+	keys map[string]Entry
+	m    sync.Mutex
+}
+
+func New() *InMemStore {
+	return &InMemStore{
+		keys: make(map[string]Entry),
+		m:    sync.Mutex{},
+	}
+}
