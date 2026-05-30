afef817a3f8e58641b6dbf9a9697ce518e36af08
eben-vranken <vranken.eben@outlook.com>
2026-05-30 23:45:11 +0200

    test: verify 422 is returned when idempotency key is reused with different request body

--- idempo_test.go
+++ idempo_test.go
@@ -151,3 +151,38 @@ func TestHandlerReplayResponse(t *testing.T) {
 		t.Errorf("Idempotency Returned = %s, extpected %s", rec2.Header().Get("Idempotency-Replayed"), "true")
 	}
 }
+
+func TestHandlerReplayResponseWithMismatchedBody(t *testing.T) {
+	jsonRequest := []byte(`{order_id:123, "status": "created"}`)
+
+	req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(jsonRequest))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Idempotency-Key", "019e705d-bb1a-7085-9c1b-58a6a14a1aeb")
+	rec := httptest.NewRecorder()
+
+	var body []byte
+
+	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, _ = io.ReadAll(r.Body)
+		w.WriteHeader(http.StatusCreated)
+		w.Write(body)
+	})
+
+	m := idempo.New(inmem.New(24 * time.Hour))
+	handler := m.Handler(next)
+	handler.ServeHTTP(rec, req)
+
+	differentJsonRequest := []byte(`{order_id:124, "status": "created"}`)
+
+	req = httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(differentJsonRequest))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Idempotency-Key", "019e705d-bb1a-7085-9c1b-58a6a14a1aeb")
+	rec2 := httptest.NewRecorder()
+
+	handler = m.Handler(next)
+	handler.ServeHTTP(rec2, req)
+
+	if rec2.Code != 422 {
+		t.Errorf("Code returned = %08b, extpected %08b", rec2.Code, 422)
+	}
+}
