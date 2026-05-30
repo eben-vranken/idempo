4cc4b041579f6f7e1094eb0a05928d8e534693fd
eben-vranken <vranken.eben@outlook.com>
2026-05-31 00:46:40 +0200

    feat: add Redis store scaffold with Complete method

--- redis/store.go
+++ redis/store.go
@@ -1,1 +1,36 @@
 package redis
+
+import (
+	"context"
+	"time"
+
+	goredis "github.com/redis/go-redis/v9"
+)
+
+type RedisStore struct {
+	client *goredis.Client
+	ttl    time.Duration
+}
+
+func (rs *RedisStore) Complete(ctx context.Context, key string, statusCode int, body []byte) error {
+	hashSetObj := rs.client.HSet(ctx, key, "state", "completed", "responseCode", statusCode, "responseBody", body)
+
+	if hashSetObj.Err() != nil {
+		return hashSetObj.Err()
+	}
+
+	expireObj := rs.client.Expire(ctx, key, rs.ttl)
+
+	if expireObj.Err() != nil {
+		return expireObj.Err()
+	}
+
+	return nil
+}
+
+func New(opt *goredis.Options, expireDuration time.Duration) *RedisStore {
+	return &RedisStore{
+		client: goredis.NewClient(opt),
+		ttl:    expireDuration,
+	}
+}
